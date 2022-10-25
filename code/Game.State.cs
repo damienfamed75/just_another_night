ba78d6cb003bbbd52a1eebc93556e097483328d3
damienfamed75 <thedoubledimensions@gmail.com>
2022-10-24 21:39:51 -0500

    Updated game loop states

--- code/Game.State.cs
+++ code/Game.State.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Sandbox;
 
@@ -15,20 +16,92 @@ public partial class JustAnotherGame
 	public enum GameStates
 	{
 		FirstCustomer,
-		FirstScare,
-		CustomerComeBack,
-		SecondScare,
+		WeirdCustomer,
+		SecondNormalCustomerAndWeirdGuyStaring,
+		BackDoorOpens,
+		FreezerUnlocks
 	}
 
 	private async Task GameLoopAsync()
 	{
-		GameState = GameStates.FirstCustomer;
-		StateTimer = Rand.Float( 30.0f, 60.0f );
+		StateTimer = 5.0f;
 		await WaitStateTimer();
 
-		// initiate first customer
+		// There should only be a single client since this is a singleplayer game.
+		var client = All.OfType<Client>().First();
+		var player = client.Pawn as JustAnotherPlayer;
 
-		GameState = GameStates.FirstScare;
+		while (player.State == JustAnotherPlayer.PlayerStates.PickupTrash) {
+			StateTimer = 1;
+			await WaitStateTimer();
+		}
+
+		GameState = GameStates.FirstCustomer;
+		StateTimer = Rand.Float( 10.0f, 15.0f );
+		await WaitStateTimer();
+
+		// Initiate first customer
+
+		while (player.State == JustAnotherPlayer.PlayerStates.TakeOutTrashBags) {
+			StateTimer = 1;
+			await WaitStateTimer();
+		}
+
+		GameState = GameStates.WeirdCustomer;
+		StateTimer = Rand.Float( 10.0f, 15.0f );
+		await WaitStateTimer();
+
+		// initiate weird customer.
+
+		while (player.State == JustAnotherPlayer.PlayerStates.WashDishes) {
+			StateTimer = 1;
+			await WaitStateTimer();
+		}
+
+		GameState = GameStates.SecondNormalCustomerAndWeirdGuyStaring;
+		StateTimer = Rand.Float( 10.0f, 15.0f );
+		await WaitStateTimer();
+
+		// initiate second normal customer.
+
+		StateTimer = Rand.Float( 20.0f, 25.0f );
+		await WaitStateTimer();
+
+		// initiate customer staring through windows.
+
+		while (player.State == JustAnotherPlayer.PlayerStates.MopFloors) {
+			StateTimer = 1;
+			await WaitStateTimer();
+		}
+
+		GameState = GameStates.BackDoorOpens;
+		StateTimer = Rand.Float( 0.0f, 1.0f );
+		await WaitStateTimer();
+
+		// initiate back door opening.
+
+		while (player.State == JustAnotherPlayer.PlayerStates.FixIceCreamMachine) {
+			StateTimer = 1;
+			await WaitStateTimer();
+		}
+
+		GameState = GameStates.FreezerUnlocks;
+		StateTimer = Rand.Float( 1.0f, 2.0f );
+		await WaitStateTimer();
+
+		// initiate freezer door opening and guy waiting in freezer.
+
+		// Find the freezer door. The map should only have one.
+		var freezerDoor = All.OfType<ControlledDoor>()
+			.Where(x => x.Tags.Has("freezer_door"))
+			.First();
+
+		// Unlock the freezer door to open it.
+		freezerDoor.Locked = false;
+		// Open the door using the player so the sound plays in their ears.
+		freezerDoor.OnUse( player );
+		// Lock the door again so the player cannot use it.
+		freezerDoor.Locked = true;
 	}
 
 	private async Task WaitStateTimer()
